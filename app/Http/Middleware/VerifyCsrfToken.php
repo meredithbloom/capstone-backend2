d030e431bca235f9e6bb11b24fddaf1f4c04e5bd
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-04-03 17:26:03 -0400

    messed around with cors routes to allow login and register

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        '/http://localhost:8000/api/register',
+        'http://localhost:8000/api/login'
     ];
 }
