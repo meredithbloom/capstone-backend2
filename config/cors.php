45673b512c15b29515a0b7d0ba23aa3fe1782d8d
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-04-04 23:36:53 -0400

    please work omfg

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['localhost', '127.0.0.1:3000', 'http://localhost:3000', '127.0.0.1:8000', 'http://localhost:8000'],
+    'allowed_origins' => ['localhost', '127.0.0.1:3000', 'http://localhost:3000', '127.0.0.1:8000'],
 
     'allowed_origins_patterns' => [],
 
